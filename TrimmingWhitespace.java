0e4c6db527e5d096d3e9e5ec5e2b6fb27d8ca73c
akshithareddyk3 <kattaakshithareddy@gmail.com>
2025-08-02 11:13:33 +0530

    Add files via upload

---
+++ TrimmingWhitespace.java
@@ -0,0 +1,9 @@
+package oops_skill;
+
+public class TrimmingWhitespace {
+	public static void main(String[] args) {
+        String s = "  Hello  ";
+        System.out.println("[" + s.trim() + "]");     
+    }
+
+}
