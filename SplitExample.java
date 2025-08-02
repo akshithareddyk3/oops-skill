acb8cf17c80588e5b39395f113bc65d70a9fac0e
akshithareddyk3 <kattaakshithareddy@gmail.com>
2025-08-02 11:25:06 +0530

    Add files via upload

---
+++ SplitExample.java
@@ -0,0 +1,12 @@
+package oops_skill;
+
+public class SplitExample {
+	public static void main(String[] args) {
+        String s = "apple,banana,grape,orange";
+        String[] fruits = s.split(",");
+        for(String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+
+}
